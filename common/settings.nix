b476041fd6bb4a1dd8b7ec0d8fdc48a07400aab7
Steve Roche <steve.roche.xyz@proton.me>
2025-11-08 13:11:25 +1100

    nix.trusted-users

--- common/settings.nix
+++ common/settings.nix
@@ -4,6 +4,7 @@
   nix.settings = {
     experimental-features = ["nix-command" "flakes"];
     substituters = ["https://hyprland.cachix.org"];
+    trusted-users = ["steve"];
     trusted-substituters = ["https://hyprland.cachix.org"];
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
